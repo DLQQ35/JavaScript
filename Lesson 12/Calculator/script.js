db828d881f3b25e86b596bea0f541904689b5a53
DLQQ35 <architkhanna260912@gmail.com>
2025-04-04 21:05:49 +0530

    Lesson 12

---
+++ Lesson 12/Calculator/script.js
@@ -0,0 +1,9 @@
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+function setScreenValue(value) {
+    document.getElementById("result").value += value;
+}
+function calculateResult() {
+    
+}
